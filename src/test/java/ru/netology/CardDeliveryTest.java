0153c3381789633b575ddb81f2c2bf8ac7d10115
Andrey <bawoper@gmail.com>
2025-03-31 22:43:28 +0300

    first commit

---
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -0,0 +1,37 @@
+package ru.netology;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import org.apache.commons.exec.CommandLine;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selenide.$;
+
+
+public class CardDeliveryTest {
+    private String generateDate (long addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }    
+
+    @Test
+    public void checkRegistration() {
+        Selenide.open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Москва");
+        String planingDate = generateDate(4, "dd.MM.yyyy");
+        $("[data-test-id='date'] input").press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(planingDate);
+        $("[data-test-id='name'] input").setValue("Прохоров Петр");
+        $("[data-test-id='phone'] input").setValue("+79679878686");
+        $("[data-test-id='agreement']").click();
+        $("button.button").click();
+        $(".notification__content").shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planingDate));
+
+    }
+}
